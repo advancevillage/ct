46e0351a4eef44899ec0fd9c5285576f34e35a3e
richard.sun <richard.sun@ucloud.cn>
2022-05-30 01:44:10 +0800

    #richard.sun: 逻辑框架 初步

--- bpf/ct.bpf.c
+++ bpf/ct.bpf.c
@@ -1,3 +1,4 @@
+#include <linux/stringify.h>
 #include <linux/bpf.h>
 #include <linux/in.h>
 #include <linux/if_ether.h>
@@ -7,202 +8,182 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
-//存储表控制参数
-#define     CT_MAP_TCP          ct_tcp
-#define     CT_MAP_ANY4         ct_any4
-#define     CT_MAP_SIZE_ANY     4096 
-#define     CT_MAP_SIZE_TCP     4096 
+struct {
+   __uint(type,         BPF_MAP_TYPE_PROG_ARRAY);
+   __type(key,          0x04);
+   __type(value,        0x04);
+   __uint(max_entries,  30);
+} jt SEC(".maps");      //jump table
 
+#define PROG(F) SEC("xdp/"__stringify(F)) int bf_##F
 
-//常量定义
-#define CT_EGRESS           0x0
-#define CT_INGRESS          0x1
+#define ingress       0
+#define prs_eth       1
+#define prs_ipv4      2
+#define egress        29
 
-//路由指令
-#define CT_TX               0x0
-#define CT_DROP             0x1
-#define CT_PASS             0x2
-
-
-//连接跟踪状态
-#define     CT_NEW              (1 << 0)
-#define     CT_ESTABLISHED      (1 << 1)
-#define     CT_REPLY            (1 << 2)
-#define     CT_RELATED          (1 << 3)
-
-//////////////////////////////////////////////////////////////
-
-//连接跟踪元组信息 从cilium移植 cilium-1.11.5/bpf/lib/common.h
-struct ipv4_ct_tuple {
-	/* Address fields are reversed, i.e.,
-	 * these field names are correct for reply direction traffic.
-	 */
-	__be32		daddr;
-	__be32		saddr;
-	/* The order of dport+sport must not be changed!
-	 * These field names are correct for original direction traffic.
-	 */
-	__be16		dport;
-	__be16		sport;
-	__u8		nexthdr;
-	__u8		flags;
+struct flow {
+	__be32  sip;
+    __be32  dip;
+    __be32  sport:16,
+            dport:16;
+    __u32   proto:8,
+            delta:8,
+            bytes:16;
 } __packed;
 
-//连接跟踪元组数据
-struct ct_entry {
-	__u64 rx_packets;
-	__u64 rx_bytes;
-	__u64 tx_packets;
-	__u64 tx_bytes;
-	__u32 lifetime;
-	/* In the kernel ifindex is u32, so we need to check in cilium-agent
-	 * that ifindex of a NodePort device is <= MAX(u16).
+//xdp-0  进入XDP处理程序
+/*
+ * bpf_xdp_adjust_meta / bpf_xdp_adjust_tail / bpf_xdp_adjust_head
+ * SourceCode: https://elixir.bootlin.com/linux/v5.4.153/source/net/core/filter.c#L3429
+ *
+ *   struct xdp_buff {
+ *      void *data;
+ *      void *data_end;
+ *      void *data_meta;
+ *      void *data_hard_start;
+ *      struct xdp_rxq_info *rxq;
+ *   };
+ *
+ * 重点一:
+ *  data_hard_start <= data_meta <= data < data_end
+ * 重点二:
+ *  data 和 data_meta 是相对位置，如果data变化，那么data_meta随之改变
+ *  eg:
+ *  data_meta = db0e4f0 , data = db0e500 
+ *                      |
+ *  data_meta = db0e4fe , data = db0e50e
+ * 重点三:
+ *  bpf_xdp_adjust_head 修改ctx地址, 赋值操作必须在其之前
+ *
+*/
+PROG(ingress)(struct xdp_md *ctx) {     
+    
+    bpf_printk("enter prs ingress 1 %x %x %x", ctx->data_meta, ctx->data, sizeof(struct flow));
+    // 封装flow
+	/* Reserve space in-front of data pointer for our meta info.
+	 * (Notice drivers not supporting data_meta will fail here!)
 	 */
-	__u16 ifindex;
-
-	/* *x_flags_seen represents the OR of all TCP flags seen for the
-	 * transmit/receive direction of this entry.
-	 */
-	__u8  tx_flags_seen;
-	__u8  rx_flags_seen;
-};
-
-//////////////////////////////////////////////////////////////
-
-// 存储表
-struct {
-   __uint(type,         BPF_MAP_TYPE_HASH);
-   __type(key,          sizeof(struct ipv4_ct_tuple));
-   __type(value,        sizeof(struct ct_entry));
-   __uint(max_entries,  CT_MAP_SIZE_TCP);
-   __uint(map_flags,    BPF_F_NO_PREALLOC);
-} CT_MAP_TCP SEC(".maps");
-
-struct {
-   __uint(type,         BPF_MAP_TYPE_HASH);
-   __type(key,          sizeof(struct ipv4_ct_tuple));
-   __type(value,        sizeof(struct ct_entry));
-   __uint(max_entries,  CT_MAP_SIZE_ANY);
-   __uint(map_flags,    BPF_F_NO_PREALLOC);
-} CT_MAP_ANY4 SEC(".maps");
-
-
-//函数连接跟踪逻辑 parser--> match action --> deparser
-static __inline __u8 ct(struct xdp_md *ctx, __u8 dir) {
-    // 定义返回路由指示
-    __u8 r = CT_TX;
-
-    // 解析报文
-    void *data_end  = (void *)(long)ctx->data_end;
-	void *data      = (void *)(long)ctx->data;
-
-    // 解析以太网L2协议
-	struct ethhdr   *eth = data;  
-
-    __u64 nh_off;       //字节偏移
-
-    nh_off = (char*)(eth + 1) - (char*)eth;
-    if (data + nh_off > data_end) {
-        r = CT_DROP;
-        goto leave;
+    int r = bpf_xdp_adjust_meta(ctx, 0 - (int)sizeof(struct flow));
+    if (r) {
+        bpf_printk("bpf_xdp_adjust_meta errno = %x", r);
+        return XDP_DROP;
     }
 
-    __u16 h_proto;      //L3 协议类型
-    h_proto = eth->h_proto; 
+	/* Notice: Kernel-side verifier requires that loading of
+	 * ctx->data MUST happen _after_ helper bpf_xdp_adjust_meta(),
+	 * as pkt-data pointers are invalidated.  Helpers that require
+	 * this are determined/marked by bpf_helper_changes_pkt_data()
+	 */
+    void *data      = (void *)(unsigned long)ctx->data;
+    void *data_end  = (void *)(unsigned long)ctx->data_end;
+    struct flow *f  = (void *)(unsigned long)ctx->data_meta;
+
+    if ((char*)(f + 1) > (char*)data) {
+        return XDP_DROP;
+    }
+
+    // 初始化Flow
+    f->sip      = 0;
+    f->dip      = 0;
+    f->sport    = 0;
+    f->dport    = 0;
+    f->proto    = 0;
+    f->delta    = 0;
+    f->bytes    = data_end - data;
+
+    // 解析以太网报文
+    bpf_tail_call(ctx, &jt, prs_eth);
+
+    // 解封flow 
+    bpf_tail_call(ctx, &jt, egress);
+
+    return XDP_PASS;
+}
+
+//xdp-29 离开XDP处理程序 
+PROG(egress)(struct xdp_md *ctx) {     
+
+    bpf_printk("enter prs egress 1 %x %x %x", ctx->data_meta, ctx->data, sizeof(struct flow));
+
+    void *data      = (void *)(unsigned long)ctx->data;
+    struct flow *f  = (void *)(unsigned long)ctx->data_meta;
+
+    if ((char*)(f + 1) > (char*)data) {
+        return XDP_DROP;
+    }
+
+    bpf_printk("sip=%x dip=%x", f->sip, f->dip);
+    bpf_printk("sport=%x dport=%x", f->sport, f->dport);
+    bpf_printk("proto=%x delta=%x bytes=%x", f->proto, f->delta, f->bytes);
+
+    bpf_printk("enter prs egress 2 %x %x %x", ctx->data_meta, ctx->data, sizeof(struct flow));
+    return XDP_PASS;
+}
+
+//xdp-1  以太网报文处理程序
+PROG(prs_eth)(struct xdp_md *ctx) { 
+
+    bpf_printk("enter prs eth 1 %x %x %x", ctx->data_meta, ctx->data, sizeof(struct flow));
+
+    void *data      = (void *)(unsigned long)ctx->data;
+    void *data_end  = (void *)(unsigned long)ctx->data_end;
+    struct flow *f  = (void *)(unsigned long)ctx->data_meta;
+
+    if ((char*)(f + 1) > (char*)data) {
+        return XDP_DROP;
+    }
+
+    struct ethhdr   *eth = data + f->delta;  
+    __u8   nh_off; 
+
+    nh_off = (char*)(eth + 1) - (char*)eth;
+    if (data + f->delta + nh_off > data_end) {
+        return XDP_DROP;
+    }
+
+    f->delta += nh_off;
+
+    __u16 h_proto = eth->h_proto; 
 
     switch (h_proto) {
     case bpf_htons(ETH_P_IP):
-        break;
-    default:
-        r = CT_PASS;
-        goto leave;
-    }
-
-    // 解析IPv4 报文
-	struct iphdr          *iph;         //L3
-
-    iph = data + nh_off;
-    nh_off += (char*)(iph + 1) - (char*)iph;
-
-    if (data + nh_off > data_end) {
-         r = CT_DROP;
-        goto leave;
-    }
-
-    // 分片报文不支持
-    if (iph->frag_off & 0xff3f) {
-        r = CT_PASS;
-        goto leave;
-    }
-
-    // 解析CT Flow数据
-    switch (iph->protocol) { //TODO: flow状态分析 NEW RELATED 
-    case 0x01: //icmp
-        //r = handle_ping()
-        break;
-    case 0x06: //tcp
-        //r = handle_tcp()
-        break;
-        //r = handle_udp()
-    case 0x11: //udp
+        bpf_tail_call(ctx, &jt, prs_ipv4);
         break;
     }
-
-leave:
-    return r;
+    
+    bpf_tail_call(ctx, &jt, egress);
+    return XDP_PASS;
 }
 
-SEC("xdp_ct_ingress")
-int ct_ingress_wan(struct xdp_md *ctx) {
-    int  xdp_r = XDP_TX;
-    __u8 r = ct(ctx, CT_INGRESS);
+//xdp-2 IPv4报文处理程序
+PROG(prs_ipv4)(struct xdp_md *ctx) { 
 
-    switch (r) {
-    case CT_DROP:
-        xdp_r = XDP_DROP;
-        break;
+    bpf_printk("enter prs ipv4 1 %x %x %x", ctx->data_meta, ctx->data, sizeof(struct flow));
 
-    case CT_PASS:
-        xdp_r = XDP_PASS;
-        break;
+    void *data      = (void *)(unsigned long)ctx->data;
+    void *data_end  = (void *)(unsigned long)ctx->data_end;
+    struct flow *f  = (void *)(unsigned long)ctx->data_meta;
 
-    case CT_TX:
-        xdp_r = XDP_TX;
-        break;
-
-    default: 
-        xdp_r = XDP_DROP;
+    if ((char*)(f + 1) > (char*)data) {
+        return XDP_DROP;
     }
 
-    return xdp_r;
-}
+	struct iphdr    *iph = data + f->delta; 
+    __u8   nh_off; 
 
-SEC("xdp_ct_egress")
-int ct_egress_lan(struct xdp_md *ctx) {
-    int  xdp_r = XDP_TX;
-    __u8 r = ct(ctx, CT_EGRESS);
-
-    switch (r) {
-    case CT_DROP:
-        xdp_r = XDP_DROP;
-        break;
-
-    case CT_PASS:
-        xdp_r = XDP_PASS;
-        break;
-
-    case CT_TX:
-        xdp_r = XDP_TX;
-        break;
-
-    default: 
-        xdp_r = XDP_DROP;
+    nh_off = (char*)(iph + 1) - (char*)iph;
+    if (data + f->delta + nh_off > data_end) {
+        return XDP_DROP;
     }
 
-    return xdp_r;
+    f->sip    = iph->saddr;
+    f->dip    = iph->daddr;
+    f->delta += nh_off;
+
+    bpf_tail_call(ctx, &jt, egress);
+    return XDP_PASS;
 }
+
 
 char _license []SEC("license") = "GPL";
-
-//clang -g -Wall -O2 -c -target bpf -D__TARGET_ARCH_x86 ct.bpf.c -I/usr/include/x86_64-linux-gnu/ -o ct.bpf.o 
-//bpftool prog loadall ct.bpf.o /sys/fs/bpf/global
